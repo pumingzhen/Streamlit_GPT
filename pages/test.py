dfa5c494f848a149fc3e76c2a536ac335d48d9b7
KamiPasi <92498495+KamiPasi@users.noreply.github.com>
2023-08-26 14:26:56 +0800

    test

--- pages/test.py
+++ pages/test.py
@@ -1,30 +1,21 @@
 import streamlit as st
 from streamlit_javascript import st_javascript
-import json
+
 import time
 
 
-def get_from_local_storage(k, out=[]):
-    if k not in st.session_state:
-        with st.spinner(f"Load {k}"):
-            v = st_javascript(f"JSON.parse(localStorage.getItem('{k}'));")
-            time.sleep(0.5)
-        return v or out
-    else:
-        return st.session_state[k]
+
+test = st.text_input("input:")
 
 
-def set_to_local_storage(k):
-    jdata = json.dumps(st.session_state[k])
+if st.button("Set"):
     st_javascript(
-        f"localStorage.setItem('{k}', JSON.stringify({jdata}));")
+        f"localStorage.setItem('test123', JSON.stringify({test}));")
     st.success("Success")
 
-test = st.text_input("input:", on_change=set_to_local_storage, args=("test_input", ), key="test_input")
-
-
-
-load = st.button("load")
-if load:
-    st.session_state['test'] = get_from_local_storage("test_input")
-    st.write(st.session_state['test'])
+if st.button("load"):
+    st.write(f"Load test")
+    v = st_javascript(f"JSON.parse(localStorage.getItem('test123'));")
+    time.sleep(0.5)
+    st.write(v)
+    st.success("Success")
